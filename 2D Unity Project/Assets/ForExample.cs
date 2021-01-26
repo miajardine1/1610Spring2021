bd1efaa8a87ad6034b7925c1fd9c3684c45e89b6
Miriam Jardine <miajardine1@gmail.com>
2021-01-25 21:13:06 -0700

    Beginner Scripting
    First 5 Tutorials

---
+++ 2D Unity Project/Assets/ForExample.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ForExample : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+
+}
+  

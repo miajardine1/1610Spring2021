78558c084a3a23f7b9627fa0f52f6474562ef84e
Miriam Jardine <miajardine1@gmail.com>
2021-01-27 10:57:12 -0700

    Beginner Scripting 9-16
    tutorials 9-16

---
+++ 2D Unity Project/Assets/ActiveObjects.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveObjects : MonoBehaviour
+{
+    void Start ()
+    {
+        gameObject.SetActive(false);
+    }
+}

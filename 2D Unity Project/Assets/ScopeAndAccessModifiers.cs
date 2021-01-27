31bd17852df2f88ba748081159d6127c0d7a00a2
Miriam Jardine <miajardine1@gmail.com>
2021-01-27 10:20:53 -0700

    Beginner Scripting 6-8
    Tutorials 6-8

---
+++ 2D Unity Project/Assets/ScopeAndAccessModifiers.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeAndAccessModifers : MonoBehaviour
+{
+    public int alpha = 5;
+
+
+    private int beta = 0;
+    private int gamma = 5;
+
+
+    private AnotherClass myOtherClass;
+
+
+    void Start()
+    {
+        int answer;
+        answer = pens * crayons * alpha;
+        Debug.Log(answer);
+    }
+
+
+    void Update()
+    {
+        Debug.Log("Alpha is set to: " + alpha)
+    }
+}

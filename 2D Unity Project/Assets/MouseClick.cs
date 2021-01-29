e4ad13acb51713e92b86384cbbe09c1846a42a4b
Miriam Jardine <miajardine1@gmail.com>
2021-01-28 22:15:03 -0700

    Beginner Scripting 17-24
    Tutorials 17-24

---
+++ 2D Unity Project/Assets/MouseClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+   void OnMouseDown ()
+    {
+        rigidbody.AddForce(-transform.forward * 500f);
+        rigidbody.useGravity = true;
+    }
+}

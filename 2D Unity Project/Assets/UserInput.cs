9aafe605f6940accdd353733cc680a7e598366ea
Miriam Jardine <miajardine1@gmail.com>
2021-02-01 10:49:15 -0700

    Attempting to code a Circle Sprite

---
+++ 2D Unity Project/Assets/UserInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public Rigidbody2D ballRigidbody2D;
+    public Vector2 direction;
+    public Vector 2 yDirection;
+
+
+    void Update()
+    {
+        if (UserInput.GetKey(KeyCode.RightArrow))
+        {
+            ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
+        }
+
+        if (UserInput.GetKey(KeyCode.LeftArrow))
+        {
+            ballRigidbody2D.AddForce(-direction, ForceMode2D.Force);
+        }
+
+        if (UserInput.GetKey(KeyCode.UpArrow))
+        {
+            ballRigidbody2D.AddForce(yDirection, ForceMode2D.Force);
+        }
+}

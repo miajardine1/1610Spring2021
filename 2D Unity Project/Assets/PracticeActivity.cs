77f4ec9340d76956e671e6445d6f69a748dc8375
Miriam Jardine <miajardine1@gmail.com>
2021-02-02 09:45:02 -0700

    Practice Activity

---
+++ 2D Unity Project/Assets/PracticeActivity.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeActivity : MonoBehaviour
+{
+    int myNum = 50;
+    
+    string myName = "John";
+    
+    int x = 5;
+    int y = 10;
+    Console.WriteLine(x + y);
+
+    int a = 5;
+    int b = 10;
+    int c = a + b;
+    Console.WriteLine(c);
+
+    int d = 5, e = 6, g = 50;
+    Console.WriteLine(d + e + g);
+
+    int myNum = 9;
+    double myDoubleNum = 8.99;
+    char myLetter = 'A';
+    bool myBoolean = false;
+    string myText = "Hello World";
+
+    bool yay = true;
+    bool nay = false;
+
+    string greeting = "Hello";
+    Console.WriteLine(greeting);
+
+    int myInt = 10;
+    Console.WriteLine(Convert.ToString(myInt));
+
+}

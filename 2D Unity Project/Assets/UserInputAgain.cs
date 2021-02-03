2955a051eebc9518742623c711f34af387495954
Miriam Jardine <miajardine1@gmail.com>
2021-02-03 10:41:36 -0700

    Practice Script

---
+++ 2D Unity Project/Assets/UserInputAgain.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInputAgain : MonoBehaviour
+{
+    public Rigidbody2D ballRigidbody2D;
+    public Vector2 direction;
+    public Vector2 ydirection;
+    public float speed = 3f;
+
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal") * speed;
+        ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
+
+        if (Input.GetButtonDown("Jump")
+        {
+            ballRigidbody2D.AddForce(ydirection, ForceMode2D.Force);
+        }
+    }
+}

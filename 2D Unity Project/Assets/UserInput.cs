fb8a323cf649ada466f11b81ca09f2cdaea6cd1e
Miriam Jardine <miajardine1@gmail.com>
2021-02-10 10:36:52 -0700

    UserInput

---
+++ 2D Unity Project/Assets/UserInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public float speed = 3f;
+    public float jumpForce = 20f;
+    public Rigidbody2D rigidbodyObj;
+
+    private Vector2 direction;
+
+    private void Update()
+    {
+        direction.x = speed * Input.GetAxis("Horizontal");
+        rigidbodyObj.AddForce(direction, ForceMode2D.Force);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            direction.y = jumpForce;
+        }
+
+        rigidbodyObj.AddForce(direction, ForceMode2D.Impulse);
+    }
+}

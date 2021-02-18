469c1adeaf1e0351b9033c6a010c66219c8601ba
Miriam Jardine <miajardine1@gmail.com>
2021-02-18 09:45:03 -0700

    BoolData

---
+++ 2D Unity Project/Assets/ScriptableObjects/GateKeeperBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateKeeperBehaviour : MonoBehaviour
+{
+    public BoolData keyObj;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (keyObj.value)
+        {
+            print(message: true);
+        }
+        else
+        {
+            print(message: false);
+        }
+    }
+}
---
+++ 2D Unity Project/Assets/ScriptableObjects/GateOpener.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateOpener : MonoBehaviour
+{
+    public BoolData keyObj;
+
+    private void Update()
+    {
+        if (keyObj.value)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}

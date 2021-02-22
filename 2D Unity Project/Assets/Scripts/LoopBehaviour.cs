4d5f6bde6d461cc7b5a5f7bc10bd366f824d2249
Miriam Jardine <miajardine1@gmail.com>
2021-02-22 10:41:27 -0700

    Loops

---
+++ 2D Unity Project/Assets/Scripts/LoopBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBehaviour : MonoBehaviour
+{
+    public int number, total;
+    public string[] cars;
+
+    void Start()
+    {
+        for (int i =0; i < number; i++)
+        {
+            total = i;
+            print(total);
+        }
+    }
+}

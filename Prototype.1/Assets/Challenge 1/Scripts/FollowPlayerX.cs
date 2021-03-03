7fc6e2b20c85d16ae45f68ceac974265276df981
Miriam Jardine <miajardine1@gmail.com>
2021-03-03 11:00:00 -0700

    Challenge 1

---
+++ Prototype.1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public Vector3 offset = new Vector3(0, 5 -7);
+
+
+
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}

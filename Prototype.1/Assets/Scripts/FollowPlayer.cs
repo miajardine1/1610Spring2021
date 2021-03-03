0e951abbcf64503c6cdafaa39b65f9b0699e19b5
Miriam Jardine <miajardine1@gmail.com>
2021-03-02 21:15:34 -0700

    1.3/1.4 Tutorials

---
+++ Prototype.1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5 - 7);
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Prototype.1/Assets/Scripts/PlayerController.cs
+++ Prototype.1/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     void Update()
     {
-        //Move vehicle forward.
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }

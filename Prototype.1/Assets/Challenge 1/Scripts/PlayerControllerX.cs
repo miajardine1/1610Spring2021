11cf8b9e469900a01321227fef7c8166bc63c056
Miriam Jardine <miajardine1@gmail.com>
2021-03-05 11:25:07 -0700

    Challenge 1

--- Prototype.1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Prototype.1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -24,6 +24,7 @@ public class PlayerControllerX : MonoBehaviour
         transform.Translate(Vector3.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * verticalInput * rotationSpeed * Time.deltaTime);
+       
     }
 }

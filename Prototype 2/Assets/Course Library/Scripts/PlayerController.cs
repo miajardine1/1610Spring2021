5c6f90cac41962d29c8a6aaa0c5040792304fd26
Miriam Jardine <miajardine1@gmail.com>
2021-03-15 10:49:52 -0600

    Prototype 2

---
+++ Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public float xRange = 10;
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.z);
+        }
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+    }
+}

eba9e0876c6be64b05f44648054716ce5d8903e5
Miriam Jardine <miajardine1@gmail.com>
2021-03-16 18:38:53 -0600

    Food Flight

---
+++ Prototype 2/Assets/Course Library/Scripts/MovePosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePosition : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 3;
+    private Vector3 direction = Vector3.zero;
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        direction.Set(horizontalInput * speed, 0, 0);
+        transform.Translate(direction * Time.deltaTime);
+    }
+}
--- Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject projectilePrefab;
+
     
+
     public float xRange = 10;
     void Update()
     {
@@ -15,6 +18,11 @@ public class PlayerController : MonoBehaviour
         if(transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player.
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
     }
